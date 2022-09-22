4b6ce2014d882c995dc58696ccc50d242a5796ff
Wagner M Cunha <wagnermc506@gmail.com>
2022-09-21 21:32:02 -0300

    atualizações nos scripts

--- Consultas.sql
+++ Consultas.sql
@@ -1,4 +1,23 @@
+-- --------  << Data Mining >>  ----------
+--
+--                    SCRIPT DE CONSULTAS (DML)
+--
+-- Data Criacao ...........: 20/09/2022
+-- Autor(es) ..............: João Guedes, Luiz Henrique e Wagner Cunha
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: anime_rec
+--
+-- PROJETO => 01 Base de Dados
+--         => 12 Tabelas
+--         => 01 Visao
+--
+-- ---------------------------------------------------------
+
 USE anime_rec;
+
+-- A consulta retorna um subconjunto da tabela 'assiste' com as notas dos usuários
+-- para cada anime, com animes de determinados gêneros excluídos e cujo o acompanhamento
+-- do anime está com status 'concluido'
 
 CREATE VIEW notas_de_animes_concluidos (usuario, anime, nota, nome) AS
 SELECT a.usuario, a.anime, a.nota, an.nome FROM
---
+++ apaga.sql
@@ -0,0 +1,31 @@
+-- --------  << Data Mining >>  ----------
+--
+--                    SCRIPT DE EXCLUSÃO (DDL)
+--
+-- Data Criacao ...........: 16/09/2022
+-- Autor(es) ..............: João Guedes, Luiz Henrique e Wagner Cunha
+-- Banco de Dados .........: MySQL 8.0
+-- Base de Dados (nome) ...: anime_rec
+--
+-- PROJETO => 01 Base de Dados
+--         => 12 Tabelas
+--         => 01 Visao
+--
+-- ---------------------------------------------------------
+
+use anime_rec;
+
+DROP TABLE assiste;
+DROP TABLE participa;
+DROP TABLE anima;
+DROP TABLE licencia;
+DROP TABLE produz;
+DROP TABLE ACOMPANHAMENTO;
+DROP TABLE GENERO;
+DROP TABLE ESTUDIO;
+DROP TABLE LICENCIADOR;
+DROP TABLE PRODUTOR;
+DROP TABLE USUARIO;
+DROP TABLE ANIME;
+
+DROP VIEW notas_de_animes_concluidos;
